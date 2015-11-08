eaf41ad2e0fbc896ada8999c8e1f3f6063a5e22a
hastimal <hjdk4@mail.umkc.edu>
2015-11-08 04:04:17 -0600

    Adding for Spark as an activity with stt

--- NurseToGoClient/app/src/main/java/com/umkc/hastimal/nursetogo/MainActivity.java
+++ NurseToGoClient/app/src/main/java/com/umkc/hastimal/nursetogo/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -17,14 +18,27 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 
     Intent intent = null;
+    Intent intent1 = null;
     Context context = null;
+
+
     TextView textResponse;
     EditText editTextAddress, editTextPort;
-    Button buttonConnect, buttonClear;
+    Button buttonConnect, buttonClear,buttonToSpark;
+    //for spark
+    //for direct text
 
+    /**
+     * public void onCreate(Bundle savedInstanceState) {
+     super.onCreate(savedInstanceState);
+     StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+     StrictMode.setThreadPolicy(policy);
+     setContentView(R.layout.main);*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
         setContentView(R.layout.activity_main);
         context = this;
         editTextAddress = (EditText)findViewById(R.id.address);
@@ -32,9 +46,12 @@ public class MainActivity extends Activity {
         buttonConnect = (Button)findViewById(R.id.connect);
         buttonClear = (Button)findViewById(R.id.clear);
         textResponse = (TextView)findViewById(R.id.response);
+        //for Spark initialization
+        //For direct local
+        buttonToSpark = (Button)findViewById(R.id.buttonToSpark);
 
         buttonConnect.setOnClickListener(buttonConnectOnClickListener);
-
+        buttonToSpark.setOnClickListener(buttonToSparkOnClickListener);
         buttonClear.setOnClickListener(new OnClickListener(){
 
             @Override
@@ -42,6 +59,7 @@ public class MainActivity extends Activity {
                 textResponse.setText("");
             }});
     }
+
 
     OnClickListener buttonConnectOnClickListener =
             new OnClickListener(){
@@ -58,7 +76,19 @@ public class MainActivity extends Activity {
                     }
                     else
                     {
-                        textResponse.setText("IP Address field is empty");
+                        textResponse.setText("IP Address of iOS field is empty");
                     }
                 }};
+    OnClickListener buttonToSparkOnClickListener =
+            new OnClickListener(){
+
+                @Override
+                public void onClick(View v) {
+
+                    Intent i = new Intent(context, ThirdActivity.class);
+                    startActivity(i);
+                }
+            };
+
+
 }
