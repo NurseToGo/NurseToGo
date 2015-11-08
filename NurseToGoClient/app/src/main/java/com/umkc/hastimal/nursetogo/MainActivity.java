c3a95463ee94690f716c0405cf770c13d886ab7b
hastimal <hjdk4@mail.umkc.edu>
2015-11-08 04:45:36 -0600

    Commented

--- NurseToGoClient/app/src/main/java/com/umkc/hastimal/nursetogo/MainActivity.java
+++ NurseToGoClient/app/src/main/java/com/umkc/hastimal/nursetogo/MainActivity.java
@@ -37,6 +37,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //This is used to fix FW in strict mode
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
         setContentView(R.layout.activity_main);
